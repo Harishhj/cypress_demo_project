8b5c6136cb144013d73332a5ad9e3f15139e3372
Harishhj <harish.hj@kapiva.in>
2024-08-20 16:18:44 +0530

    updated the locator10

--- cypress/e2e/Test/firstTest.cy.js
+++ cypress/e2e/Test/firstTest.cy.js
@@ -1,62 +1,50 @@
 ///<reference types="Cypress"/>
 
-// const closeOverlayIfVisible = () => {
-//     cy.get('div.fixed.inset-0.z-[100]').then(($overlay) => {
-//         if ($overlay.length > 0) {
-//             cy.get('div.fixed.inset-0.z-[100]').invoke('remove'); // Or perform other actions to handle overlay
-//         }
-//     });
-// };
-
-
 describe('Kapiva login', () => {
     beforeEach(() => {
         cy.viewport(360, 667)
-        cy.visit('https://manmatters.com/')
+        cy.setCookie('region', 'IN')
+        cy.visit('https://kapiva.in/')
         cy.wait(10000)
     })
     it('Verify the Login flow', () => {
-        cy.get('[class="styles__HamburgerWarpper-sc-1qvuluh-0 hvMRYt"]')
-        .should('be.visible')
-        .click()
-        cy.task('log', 'clicked on hamberger menu successfully')
-
-        cy.get('a[href="#"]')
-        .should('be.visible')
-        .click()
-        cy.task('log', 'clicked on login button successfully')
-
-
-        
-        // //closeOverlayIfVisible(); // Handle overlay if needed
-        // cy.get('div.fixed.inset-0.z-[100]')
-        // .find('button.h-full.px-1.lg\\:order-2')
-        // .scrollIntoView()
+        // cy.get('[class="styles__HamburgerWarpper-sc-1qvuluh-0 hvMRYt"]')
         // .should('be.visible')
-        // .click({force:true})
-        // // cy.get('button.h-full.px-1.lg\\:order-2').scrollIntoView().should('be.visible');
-        // // cy.get('button.h-full.px-1.lg\\:order-2').click({ force: true })
-
+        // .click()
         // cy.task('log', 'clicked on hamberger menu successfully')
 
-        // cy.get("span[class ='text-white hover:text-white']")
-        //     .should('be.visible')
-        //     .click()
+        // cy.get('a[href="#"]')
+        // .should('be.visible')
+        // .click()
         // cy.task('log', 'clicked on login button successfully')
 
-        // cy.get('#outlined-basic')
-        //     .should('exist')
-        //     .type('9480507672')
-        // cy.task('log', 'entered phone number successfully')
 
-        // cy.get('[id=":r1:"]')
-        //     .should('be.enabled')
-        //     .click()
-        // cy.task('log', 'clicked on submit button successfully')
 
-        // cy.get("input[aria-label='Please enter verification code. Digit 1']")
-        //     .should('be.visible')
-        //     .type('676767')
-        // cy.task('log', 'entered OTP successfully')
+
+        cy.get('button.h-full.px-1.lg\\:order-2')
+            .scrollIntoView()
+            .should('be.visible')
+            .click()
+        cy.task('log', 'clicked on hamberger menu successfully')
+
+        cy.get("span[class ='text-white hover:text-white']")
+            .should('be.visible')
+            .click()
+        cy.task('log', 'clicked on login button successfully')
+
+        cy.get('#outlined-basic')
+            .should('exist')
+            .type('9480507672')
+        cy.task('log', 'entered phone number successfully')
+
+        cy.get('[id=":r1:"]')
+            .should('be.enabled')
+            .click()
+        cy.task('log', 'clicked on submit button successfully')
+
+        cy.get("input[aria-label='Please enter verification code. Digit 1']")
+            .should('be.visible')
+            .type('676767')
+        cy.task('log', 'entered OTP successfully')
     })
 })
